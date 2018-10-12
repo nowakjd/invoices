fd9ef3c723a262c6f40a5c6289bc5b688d3a150a
michalprzepiora <michal.przepiora@gmail.com>
2018-10-12 19:45:42 +0200

    Vat enum

--- src/test/java/VatTest.java
+++ src/test/java/VatTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class VatTest {
 
   @Test
-  void getVatRates() {
+  void checkGetVatRates() {
     assertEquals(0, Vat.RATE_0.getValue());
     assertEquals(0.05f, Vat.RATE_5.getValue());
     assertEquals(0.08f, Vat.RATE_8.getValue());
