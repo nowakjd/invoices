ee7eefafc5079cc9e54569f902ad453ca346a05c
michalprzepiora <michal.przepiora@gmail.com>
2018-10-12 19:43:34 +0200

    Vat enum

---
+++ src/main/java/Vat.java
@@ -0,0 +1,16 @@
+public enum Vat {
+  RATE_0(0f),
+  RATE_5(0.05f),
+  RATE_8(0.08f),
+  RATE_23(0.23f);
+
+  private float value;
+
+  Vat(float value) {
+    this.value = value;
+  }
+
+  public float getValue() {
+    return value;
+  }
+}
