1bba5347125d5a2d8d76a3c7568792c9be8e0626
jedrzej <JedrzejNowak>
2018-10-14 09:22:27 +0200

    Creating Database Interface

---
+++ src/main/java/pl/coderstrust/database/Database.java
@@ -0,0 +1,18 @@
+package pl.coderstrust.database;
+
+import pl.coderstrust.model.Invoice;
+
+import java.util.Collection;
+
+
+public interface Database {
+
+  void saveInvoice(Invoice invoice);
+
+  Collection<Invoice> getInvoices();
+
+  void updateInvoice(Invoice invoice);
+
+  void deleteInvoiceById(int id);
+
+}
