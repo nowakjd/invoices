6257409f788530aefe07719a8b35b10701192433
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-10-15 23:56:28 +0200

    Project #2 Invoice/InvoiceTest fix implementation

--- src/test/java/pl/coderstrust/model/InvoiceTest.java
+++ src/test/java/pl/coderstrust/model/InvoiceTest.java
@@ -3,46 +3,58 @@ package pl.coderstrust.model;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 import java.util.List;
 
 class InvoiceTest {
 
-  private static final String NUMBER = "FV 2018/01/12345";
-  private static final LocalDate ISSUE_DATE = LocalDate.of(2018, 11, 1);
+  private static final long ID = 1;
+  private static final String ISSUE = "2018/01/1234567";
+  private static final LocalDate ISSUE_DATE = LocalDate.of(2018, 10, 1);
   private static final Company SELLER = null;
   private static final Company BUYER = null;
   private static final List<InvoiceEntry> ENTRIES = null;
 
   @Test
-  void testSettersForCorrectArguments() {
-    Invoice invoice = new Invoice();
-    invoice.setNumber(NUMBER);
-    invoice.setIssueDate(ISSUE_DATE);
-    invoice.setSeller(SELLER);
-    invoice.setBuyer(BUYER);
-    invoice.setEntries(ENTRIES);
+  @DisplayName("Checking getters")
+  void testGetters() {
+    Invoice invoice = new Invoice(ID, ISSUE, ISSUE_DATE, SELLER, BUYER, ENTRIES);
 
     assertAll(
-        () -> assertEquals(NUMBER, invoice.getNumber()),
+        () -> assertEquals(ID, invoice.getId()),
+        () -> assertEquals(ISSUE, invoice.getIssue()),
         () -> assertEquals(ISSUE_DATE, invoice.getIssueDate()),
         () -> assertEquals(SELLER, invoice.getSeller()),
         () -> assertEquals(BUYER, invoice.getBuyer()),
         () -> assertIterableEquals(ENTRIES, invoice.getEntries()));
   }
 
-  @Test
-  void testGetters() {
-    Invoice invoice = new Invoice(NUMBER, ISSUE_DATE, SELLER, BUYER, ENTRIES);
+  @ParameterizedTest
+  @DisplayName("Test invalid values of id")
+  @ValueSource(longs = {-1, 0})
+  void invaldValueOfIdTest(long id) {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> {
+          Invoice invoice = new Invoice(id, ISSUE, ISSUE_DATE, SELLER, BUYER, ENTRIES);
+        });
+    assertEquals("The id should be greater than zero", exception.getMessage());
+  }
 
-    assertAll(
-        () -> assertEquals(NUMBER, invoice.getNumber()),
-        () -> assertEquals(ISSUE_DATE, invoice.getIssueDate()),
-        () -> assertEquals(SELLER, invoice.getSeller()),
-        () -> assertEquals(BUYER, invoice.getBuyer()),
-        () -> assertIterableEquals(ENTRIES, invoice.getEntries()));
+  @Test
+  @DisplayName("Checking date out of range")
+  void testInvalidDate() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> {
+          Invoice invoice = new Invoice(ID, ISSUE, LocalDate.of(2018, 12, 10), SELLER, BUYER,
+              ENTRIES);
+        });
+    assertEquals("Passing date cannot be in the future", exception.getMessage());
   }
 }
