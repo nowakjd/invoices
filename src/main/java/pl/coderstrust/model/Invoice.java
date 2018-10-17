4f4ec5f465c8d429962c7d04b875500f434059a2
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-10-17 19:38:17 +0200

    Project #2 Invoice/InvoiceTest fix implementation

--- src/main/java/pl/coderstrust/model/Invoice.java
+++ src/main/java/pl/coderstrust/model/Invoice.java
@@ -1,44 +1,48 @@
 package pl.coderstrust.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Invoice {
 
   private final long id;
-  private final String issue;
   private final LocalDate issueDate;
+  private final List<InvoiceEntry> entries;
+  private final String issue;
   private final Company seller;
   private final Company buyer;
-  private final List<InvoiceEntry> entries;
 
-
-  public Invoice(long id, String issue, LocalDate issueDate, Company seller,
-      Company buyer, List<InvoiceEntry> entries) {
+  public Invoice(long id, LocalDate issueDate, List<InvoiceEntry> entries, String issue,
+      Company seller, Company buyer) {
     if (id <= 0) {
       throw new IllegalArgumentException("The id should be greater than zero");
     }
     this.id = id;
-    this.issue = issue;
     if (issueDate.isAfter(LocalDate.now())) {
       throw new IllegalArgumentException("Passing date cannot be in the future");
     }
     this.issueDate = issueDate;
+    this.entries = entries != null ? new ArrayList(entries) : new ArrayList();
+    this.issue = issue;
     this.seller = seller;
     this.buyer = buyer;
-    this.entries = entries;
   }
 
   public long getId() {
     return id;
   }
 
-  public String getIssue() {
-    return issue;
-  }
-
   public LocalDate getIssueDate() {
     return issueDate;
+  }
+
+  public List<InvoiceEntry> getEntries() {
+    return entries;
+  }
+
+  public String getIssue() {
+    return issue;
   }
 
   public Company getSeller() {
@@ -47,9 +51,5 @@ public class Invoice {
 
   public Company getBuyer() {
     return buyer;
-  }
-
-  public List<InvoiceEntry> getEntries() {
-    return entries;
   }
 }
