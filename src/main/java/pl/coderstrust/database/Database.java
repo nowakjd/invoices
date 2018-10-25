251517b579ac7e1027dbc91cc1623079b02fadd4
michalprzepiora <michal.przepiora@gmail.com>
2018-10-25 21:53:22 +0200

    Update methods in interface

--- src/main/java/pl/coderstrust/database/Database.java
+++ src/main/java/pl/coderstrust/database/Database.java
@@ -10,9 +10,7 @@ public interface Database {
 
   void save(Invoice invoice) throws DatabaseOperationException;
 
-  void update(Invoice invoice) throws DatabaseOperationException;
-
-  void delete(long id) throws DatabaseOperationException;
+  void deleteById(long id) throws DatabaseOperationException;
 
   Collection<Invoice> findAll() throws DatabaseOperationException;
 
@@ -20,7 +18,7 @@ public interface Database {
 
   Collection<Invoice> findBySeller(Company company) throws DatabaseOperationException;
 
-  Collection<Invoice> find(LocalDate fromDate, LocalDate toDate) throws DatabaseOperationException;
+  Collection<Invoice> findByDate(LocalDate fromDate, LocalDate toDate) throws DatabaseOperationException;
 
-
+  Invoice findById(long id) throws DatabaseOperationException;
 }
--- src/main/java/pl/coderstrust/database/DatabaseOperationException.java
+++ src/main/java/pl/coderstrust/database/DatabaseOperationException.java
@@ -2,4 +2,7 @@ package pl.coderstrust.database;
 
 public class DatabaseOperationException extends Exception {
 
+  public DatabaseOperationException(String message) {
+    super(message);
+  }
 }
