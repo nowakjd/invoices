08f8adb6f43908acdf92f19916232c805cb84130
jedrzej <JedrzejNowak>
2018-10-27 22:26:43 +0200

    Creating JsonConverter class

---
+++ src/main/java/pl/coderstrust/database/JsonConverter.java
@@ -0,0 +1,39 @@
+package pl.coderstrust.database;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import pl.coderstrust.model.Invoice;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JsonConverter {
+
+  private ObjectMapper objectMapper = new ObjectMapper();
+
+  public JsonConverter() {
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+  }
+
+  public String convert(Invoice invoice) throws JsonProcessingException {
+    return objectMapper.writeValueAsString(invoice);
+  }
+
+  public Invoice convert(String line) throws IOException {
+    return objectMapper.readValue(line, Invoice.class);
+  }
+
+  public ArrayList<Invoice> convert(ArrayList<String> lines) throws IOException {
+    ArrayList<Invoice> result = new ArrayList<>();
+    for (String line : lines) {
+      result.add(convert(line));
+    }
+    return result;
+  }
+
+}
