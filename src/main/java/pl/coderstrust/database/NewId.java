462ed00fc0e258c22fd17cdf3aa8a7e893def65b
jedrzej <JedrzejNowak>
2018-10-27 22:23:22 +0200

    Creating NewId class

---
+++ src/main/java/pl/coderstrust/database/NewId.java
@@ -0,0 +1,52 @@
+package pl.coderstrust.database;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+class NewId {
+
+  private String idFileName;
+
+  public NewId(String idFileName) throws DatabaseOperationException {
+    this.idFileName = idFileName;
+    File file = new File(idFileName);
+    try {
+      if (!file.exists()) {
+        FileWriter fileWriter = new FileWriter(idFileName);
+        fileWriter.write(1L + System.getProperty("line.separator"));
+        fileWriter.close();
+      }
+    } catch (IOException exception) {
+      throw new DatabaseOperationException("idFile error");
+    }
+  }
+
+  public long getNewId() throws DatabaseOperationException {
+    long id;
+    FileReader fileReader;
+    Scanner scanner;
+    try {
+      fileReader = new FileReader(idFileName);
+      scanner = new Scanner(fileReader);
+      String line = (scanner.nextLine());
+      id = Long.valueOf(line);
+
+    } catch (FileNotFoundException ex) {
+      throw new DatabaseOperationException("idFile error");
+    }
+    FileWriter fileWriter;
+    try {
+      fileWriter = new FileWriter(idFileName);
+      fileWriter.write(id + 1 + "");
+      fileWriter.close();
+    } catch (IOException exception) {
+      throw new DatabaseOperationException("idFile error");
+    }
+    return id;
+
+  }
+}
