c20c1b78dac3bba87421afac3c775233d6769985
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-11-14 10:26:34 +0100

    InvoiceBook - renamed class -> InvoiceService, added test to class; added Mockito dependency to pom.xml; rearranged project structure;

--- src/main/java/pl/coderstrust/invoices/model/Invoice.java
+++
@@ -1,5 +0,0 @@
-package pl.coderstrust.invoices.model;
-
-public class Invoice {
-
-}
--- src/main/java/pl/coderstrust/service/InvoiceService.java
+++ src/main/java/pl/coderstrust/service/InvoiceService.java
@@ -1,11 +1,12 @@
 package pl.coderstrust.service;
 
-import java.time.LocalDate;
-import java.util.Collection;
 import pl.coderstrust.database.Database;
 import pl.coderstrust.database.DatabaseOperationException;
 import pl.coderstrust.model.Company;
 import pl.coderstrust.model.Invoice;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 public class InvoiceService {
 
@@ -15,32 +16,32 @@ public class InvoiceService {
     this.database = database;
   }
 
-  public void save(Invoice invoice) throws DatabaseOperationException {
-    database.save(invoice);
+  public Invoice save(Invoice invoice) throws DatabaseOperationException {
+    return database.save(invoice);
   }
 
   public void delete(Long id) throws DatabaseOperationException {
     database.delete(id);
   }
 
-  Collection<Invoice> findAll() throws DatabaseOperationException {
+  public Collection<Invoice> findAll() throws DatabaseOperationException {
     return database.findAll();
   }
 
-  Collection<Invoice> findByBuyer(Company company) throws DatabaseOperationException {
+  public Collection<Invoice> findByBuyer(Company company) throws DatabaseOperationException {
     return database.findByBuyer(company);
   }
 
-  Collection<Invoice> findBySeller(Company company) throws DatabaseOperationException {
+  public Collection<Invoice> findBySeller(Company company) throws DatabaseOperationException {
     return database.findBySeller(company);
   }
 
-  Collection<Invoice> findByDate(LocalDate fromDate, LocalDate toDate)
+  public Collection<Invoice> findByDate(LocalDate fromDate, LocalDate toDate)
       throws DatabaseOperationException {
     return database.findByDate(fromDate, toDate);
   }
 
-  Invoice findOne(Long id) throws DatabaseOperationException {
+  public Invoice findOne(Long id) throws DatabaseOperationException {
     return database.findOne(id);
   }
 }
