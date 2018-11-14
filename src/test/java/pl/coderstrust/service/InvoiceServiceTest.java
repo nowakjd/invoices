c3a8c5c51d3df74392ac4e8a0768c50a2ba1b299
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-11-14 10:27:04 +0100

    InvoiceBook - renamed class -> InvoiceService, added test to class; added Mockito dependency to pom.xml; rearranged project structure;

---
+++ src/test/java/pl/coderstrust/service/InvoiceServiceTest.java
@@ -0,0 +1,101 @@
+package pl.coderstrust.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.coderstrust.database.Database;
+import pl.coderstrust.database.DatabaseOperationException;
+import pl.coderstrust.model.Company;
+import pl.coderstrust.model.Invoice;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@ExtendWith(MockitoExtension.class)
+class InvoiceServiceTest {
+
+  private InvoiceService invoiceService;
+  private Invoice invoice;
+  private Collection<Invoice> invoiceCollection;
+  private Company company;
+  private LocalDate fromDate;
+  private LocalDate toDate;
+
+  @Mock
+  private Database database;
+
+  @BeforeEach
+  void setUp() {
+    invoiceService = new InvoiceService(database);
+    invoice = new Invoice();
+    invoiceCollection = new ArrayList<>();
+    company = new Company();
+    fromDate = LocalDate.now().minusDays(7);
+    toDate = LocalDate.now();
+  }
+
+  @Test
+  @DisplayName("Checking the save method call")
+  void saveMethodTest() throws DatabaseOperationException {
+    when(database.save(invoice)).thenReturn(invoice);
+    invoiceService.save(invoice);
+    verify(database).save(any(Invoice.class));
+  }
+
+  @Test
+  @DisplayName("Checking the delete method call")
+  void deleteMethodTest() throws DatabaseOperationException {
+    doNothing().when(database).delete(1L);
+    invoiceService.delete(1L);
+    verify(database).delete(any());
+  }
+
+  @Test
+  @DisplayName("Checking the findAll method call")
+  void findAllMethodTest() throws DatabaseOperationException {
+    when(database.findAll()).thenReturn(invoiceCollection);
+    invoiceService.findAll();
+    verify(database).findAll();
+  }
+
+  @Test
+  @DisplayName("Checking the findByBuyer method call")
+  void findByBuyerMethodTest() throws DatabaseOperationException {
+    when(database.findByBuyer(company)).thenReturn(invoiceCollection);
+    invoiceService.findByBuyer(company);
+    verify(database).findByBuyer(company);
+  }
+
+  @Test
+  @DisplayName("Checking the findBySeller method call")
+  void findBySellerMethodTest() throws DatabaseOperationException {
+    when(database.findBySeller(company)).thenReturn(invoiceCollection);
+    invoiceService.findBySeller(company);
+    verify(database).findBySeller(company);
+  }
+
+  @Test
+  @DisplayName("Checking the findByDate method call")
+  void findByDateMethodTest() throws DatabaseOperationException {
+    when(database.findByDate(fromDate, toDate)).thenReturn(invoiceCollection);
+    invoiceService.findByDate(fromDate, toDate);
+    verify(database).findByDate(fromDate, toDate);
+  }
+
+  @Test
+  @DisplayName("Checking the findOne method call")
+  void findOneMethodTest() throws DatabaseOperationException {
+    when(database.findOne(1L)).thenReturn(invoice);
+    invoiceService.findOne(1L);
+    verify(database).findOne(1L);
+  }
+}
