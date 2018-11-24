ad3fc1870e9cf4c5b3d48b5c8a4ac03f02b4eb9f
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-11-24 10:29:22 +0100

    Added InMemoryDatabase to run Application context

---
+++ src/main/java/pl/coderstrust/database/InMemoryDatabase.java
@@ -0,0 +1,48 @@
+package pl.coderstrust.database;
+
+import org.springframework.stereotype.Repository;
+import pl.coderstrust.model.Company;
+import pl.coderstrust.model.Invoice;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Repository
+public class InMemoryDatabase implements Database {
+
+  @Override
+  public Invoice save(Invoice invoice) throws DatabaseOperationException {
+    return null;
+  }
+
+  @Override
+  public void delete(Long id) throws DatabaseOperationException {
+
+  }
+
+  @Override
+  public Collection<Invoice> findAll() throws DatabaseOperationException {
+    return null;
+  }
+
+  @Override
+  public Collection<Invoice> findByBuyer(Company company) throws DatabaseOperationException {
+    return null;
+  }
+
+  @Override
+  public Collection<Invoice> findBySeller(Company company) throws DatabaseOperationException {
+    return null;
+  }
+
+  @Override
+  public Collection<Invoice> findByDate(LocalDate fromDate, LocalDate toDate)
+      throws DatabaseOperationException {
+    return null;
+  }
+
+  @Override
+  public Invoice findOne(Long id) throws DatabaseOperationException {
+    return null;
+  }
+}
