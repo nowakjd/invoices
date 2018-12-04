e77f921e388e09cfb1ecf66cac3270dae9c385f4
grzegorz.chabiera <grzegorz.chabiera@gmail.com>
2018-12-04 17:16:27 +0100

    Added ApiInfo to SwaggerConfig; @Api annotations to InvoiceController

--- src/main/java/pl/coderstrust/configuration/SwaggerConfig.java
+++ src/main/java/pl/coderstrust/configuration/SwaggerConfig.java
@@ -2,8 +2,11 @@ package pl.coderstrust.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +19,21 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("pl.coderstrust"))
+        .apis(RequestHandlerSelectors.basePackage("pl.coderstrust.controller"))
         .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Spring Boot REST API")
+        .description("\"Spring Boot REST API for Invoices App\"")
+        .version("1.0")
+        .license("Apache License Version 2.0")
+        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+        .contact(new Contact("Group 7", "https://group7.com/about",
+            "group7@email.com"))
         .build();
   }
 }
--- src/main/java/pl/coderstrust/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/controller/InvoiceController.java
@@ -1,5 +1,8 @@
 package pl.coderstrust.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,8 +20,9 @@ import pl.coderstrust.service.InvoiceService;
 import java.time.LocalDate;
 import java.util.Collection;
 
-@RequestMapping("/invoices")
 @RestController
+@RequestMapping("/invoices")
+@Api(value = "/invoices", description = "Operations related to invoices in Invoices App")
 public class InvoiceController {
 
   private InvoiceService invoiceService;
@@ -28,42 +32,84 @@ public class InvoiceController {
   }
 
   @PostMapping
-  public Invoice save(@RequestBody Invoice invoice) throws DatabaseOperationException {
+  @ApiOperation(
+      value = "Add/update an invoice",
+      notes = "Add new invoice with next id to the database. "
+          + "If id exists in the database invoice wil be updated",
+      response = Invoice.class,
+      responseContainer = "List")
+  public Invoice save(
+      @ApiParam(value = "The details of invoice to be added/updated in the database",
+          required = true) @RequestBody Invoice invoice) throws DatabaseOperationException {
     return invoiceService.save(invoice);
   }
 
   @DeleteMapping("/{id}")
-  public void delete(@PathVariable Long id)
+  @ApiOperation(
+      value = "Remove an invoice",
+      notes = "Remove invoice with given ID from the database")
+  public void delete(
+      @ApiParam(value = "The invoice ID to be deleted", required = true) @PathVariable Long id)
       throws DatabaseOperationException {
     invoiceService.delete(id);
   }
 
   @GetMapping
+  @ApiOperation(
+      value = "Find all invoices",
+      notes = "Return all invoices from the database",
+      response = Invoice.class,
+      responseContainer = "List")
   Collection<Invoice> findAll() throws DatabaseOperationException {
     return invoiceService.findAll();
   }
 
   @GetMapping("/buyer/{companyId}")
-  Collection<Invoice> findByBuyer(@PathVariable Long companyId)
-      throws DatabaseOperationException {
+  @ApiOperation(
+      value = "Find invoices by buyer",
+      notes = "Return all invoices by the buyer ID from the database",
+      response = Invoice.class,
+      responseContainer = "List")
+  Collection<Invoice> findByBuyer(
+      @ApiParam(value = "The ID of buyer based on which the list of invoices will be returned",
+          required = true) @PathVariable Long companyId) throws DatabaseOperationException {
     return invoiceService.findByBuyer(companyId);
   }
 
   @GetMapping("/seller/{companyId}")
-  Collection<Invoice> findBySeller(@PathVariable Long companyId)
-      throws DatabaseOperationException {
+  @ApiOperation(
+      value = "Find invoices by seller",
+      notes = "Return all invoices by the seller ID from the database",
+      response = Invoice.class,
+      responseContainer = "List")
+  Collection<Invoice> findBySeller(
+      @ApiParam(value = "The ID of seller based on which the list of invoices will be returned",
+          required = true) @PathVariable Long companyId) throws DatabaseOperationException {
     return invoiceService.findBySeller(companyId);
   }
 
   @GetMapping("/byDate")
-  Collection<Invoice> findByDate(@DateTimeFormat(iso = ISO.DATE) @RequestParam LocalDate fromDate,
+  @ApiOperation(
+      value = "Find invoices by range of date",
+      notes = "Return all invoices from the database in terms of given range of date",
+      response = Invoice.class,
+      responseContainer = "List")
+  Collection<Invoice> findByDate(
+      @ApiParam(value = "Starting date", required = true) @DateTimeFormat(iso = ISO.DATE)
+      @RequestParam LocalDate fromDate, @ApiParam(value = "Ending date", required = true)
       @DateTimeFormat(iso = ISO.DATE) @RequestParam LocalDate toDate)
       throws DatabaseOperationException {
     return invoiceService.findByDate(fromDate, toDate);
   }
 
   @GetMapping("/{id}")
-  Invoice findOne(@PathVariable Long id)
+  @ApiOperation(
+      value = "Find invoice by id",
+      notes = "Return invoice with given ID from the database",
+      response = Invoice.class,
+      responseContainer = "List")
+  Invoice findOne(
+      @ApiParam(value = "The invoice ID to be found", required = true) @PathVariable Long id)
       throws DatabaseOperationException {
     return invoiceService.findOne(id);
   }
