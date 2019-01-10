a728ec949af065d565f90a77739808ef254ad778
michalprzepiora <michal.przepiora@gmail.com>
2019-01-10 18:24:36 +0100

    service class done - reformat code

---
+++ src/main/java/pl/coderstrust/configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package pl.coderstrust.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/h2-console/**").hasAnyRole("ADMIN")
+        .anyRequest().authenticated()
+        .and().formLogin()
+        .and().csrf().disable().headers().frameOptions().disable();
+  }
+
+  @Autowired
+  public void securityUsers(AuthenticationManagerBuilder authentication) throws Exception {
+    authentication.inMemoryAuthentication()
+        .withUser("user").password("{noop}user").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}admin").roles("ADMIN");
+  }
+}
