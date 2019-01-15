273a3ce52aa86b4a199f699889f7bbb1a7991f29
jedrzej <JedrzejNowak>
2019-01-15 10:34:43 +0100

    rebase

--- src/test/java/pl/coderstrust/database/file/JsonConverterTest.java
+++ src/test/java/pl/coderstrust/database/file/JsonConverterTest.java
@@ -18,30 +18,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class JsonConverterTest {
+class JsonConverterTest {
 
   private static final LocalDate date = LocalDate.parse("2018-11-11");
   private static Invoice invoice1;
   private static Invoice invoice2;
-  static JsonConverter jsonConverter;
+  private static JsonConverter jsonConverter;
 
   @BeforeAll
   static void setUp() {
-    Address address = new Address("Wall Street", "12/55B", "New York", "12-999");
+    Address address = new Address(null,"Wall Street", "12/55B", "New York", "12-999");
     Company seller = new Company(1L, "Microsoft", address, "5272830422",
         "11114015601081110181488249");
     Company buyer = new Company(41L, "Netflix", address, "6570011469",
         "11114015601081110181488249");
-    InvoiceEntry invoiceEntry1 = new InvoiceEntry(10, "Lego", "piece",
+    InvoiceEntry invoiceEntry1 = new InvoiceEntry(null,10, "Lego", "piece",
         new BigDecimal(199.99).setScale(2, BigDecimal.ROUND_HALF_EVEN),
         Vat.RATE_23,
         new BigDecimal(199.99), new BigDecimal(500));
-    InvoiceEntry invoiceEntry2 = new InvoiceEntry(4, "Barbie", "piece",
+    InvoiceEntry invoiceEntry2 = new InvoiceEntry(null,4, "Barbie", "piece",
         new BigDecimal(123.11).setScale(2, BigDecimal.ROUND_HALF_EVEN),
         Vat.RATE_8,
         new BigDecimal(399.99).setScale(2, BigDecimal.ROUND_HALF_EVEN), new BigDecimal(100));
-    InvoiceEntry invoiceEntry3 = new InvoiceEntry(1, "Sand", "kg", new BigDecimal(19), Vat.RATE_0,
-        new BigDecimal(99), new BigDecimal(12));
+    InvoiceEntry invoiceEntry3 = new InvoiceEntry(null,1, "Sand", "kg",
+        new BigDecimal(19), Vat.RATE_0, new BigDecimal(99), new BigDecimal(12));
     List<InvoiceEntry> invoiceEntries = new ArrayList<>(
         Arrays.asList(invoiceEntry1, invoiceEntry2, invoiceEntry3));
     invoice1 = new Invoice(null, date, invoiceEntries, "FA/111/2018", seller, buyer);
